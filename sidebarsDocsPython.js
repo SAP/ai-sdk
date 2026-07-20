666cbe08ccb614ac99558c5e38233e3566e0bff7
BrigittaK307 <brigitta.kovacs@sap.com>
2026-07-20 13:25:32 +0300

    Added Python navbar and examples pages

---
+++ sidebarsDocsPython.js
@@ -0,0 +1,23 @@
+module.exports = {
+  docsPythonSidebar: [
+    'overview-cloud-sdk-for-ai-python',
+    {
+      type: 'category',
+      label: 'Examples',
+      items: [
+        'examples/gen-ai-hub',
+        'examples/streaming',
+        'examples/prompt-registry',
+        'examples/orchestration-service',
+        'examples/orchestration-service2',
+        'examples/document-grounding',
+        'examples/document-grounding2',
+        'examples/async-examples',
+        'examples/evaluations',
+        'examples/batch-service',
+        'examples/metering',
+        'examples/ai-vs-ai'
+      ]
+    }
+  ]
+};
