e1afa58f94826e76f1f2180806b0d624d9796590
Marika Marszalkowski <marika.marszalkowski@sap.com>
2026-08-03 20:33:04 +0200

    fix name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 import { themes } from 'prism-react-renderer';
 import webpack from 'webpack';
 import remarkEnforceMdxLinks from './plugins/remark-enforce-mdx-links.js';
-import remarkStripTwoslashCuts from './scripts/remark-strip-twoslash.js';
+import remarkStripTwoslashCuts from './scripts/remark-plugin-strip-twoslash.js';
 
 const { ProvidePlugin } = webpack;
 
